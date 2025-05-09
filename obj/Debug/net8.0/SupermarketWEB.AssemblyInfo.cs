1bf5e993fc4a8814504990980308c19ba21d8bfc
EmySalazar-G1 <emelysala8@gmail.com>
2025-05-09 18:56:23 -0500

    Index.cshtml.cs Products

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b805cc5699ad48ef267bbb43db8b4322a0c2cfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d715ea67614500af263b124ad44004e31622586a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

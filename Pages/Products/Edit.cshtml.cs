24a375f44d1cf24474d5b9553500ed8b9ddd7257
EmySalazar-G1 <emelysala8@gmail.com>
2025-05-09 22:27:51 -0500

    Clase Editar de Productos

---
+++ Pages/Products/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Edit
+{
+    public class EditModel : PageModel
+    {
+        private readonly SumermarketContext _context;
+
+        public EditModel(SumermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Products { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            Products = product;
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Products).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductsExists(Products.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage("./Index");
+
+        }
+        private bool ProductsExists(int id)
+        {
+            return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6419b334a832feca49882d92dbc910f1f84c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c7317a681e5495733d03d594dba1b2b8d87dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

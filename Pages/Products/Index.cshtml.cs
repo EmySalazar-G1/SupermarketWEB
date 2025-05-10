b48f183ebc3a905e67cc2bb65452c6312c1ba4f0
EmySalazar-G1 <emelysala8@gmail.com>
2025-05-09 19:41:32 -0500

    HTML INDEX PRODUCTS

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Products
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly SumermarketContext _context;
+
+        public IndexModel(SumermarketContext context)
         {
+            _context = context;
+        }
+
+        public IList<Product> Products { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Products = await _context.Products.ToListAsync();
+            }
         }
     }
 }
--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d715ea67614500af263b124ad44004e31622586a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf5e993fc4a8814504990980308c19ba21d8bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

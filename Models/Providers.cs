7d16b6b71e2ca4a07eb827f0769e0298d99c355b
EmySalazar-G1 <emelysala8@gmail.com>
2025-05-09 23:15:34 -0500

    Crear clase providers

---
+++ Models/Providers.cs
@@ -0,0 +1,13 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Providers
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public ICollection<Providers>? Provider { get; set; }
+    }
+}
+    
+
--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a375f44d1cf24474d5b9553500ed8b9ddd7257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53bbc933b54b6ceccbe56d78bd44945072ebfd60")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

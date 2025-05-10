80983b6e67949794f6ececdeabf1772c7f2a17b8
EmySalazar-G1 <emelysala8@gmail.com>
2025-05-10 02:12:39 -0500

    Create cs Providers

---
+++ Pages/Providers/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Razor;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SumermarketContext _context;
+        public CreateModel(SumermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        public Provider Provider { get; set; } = default!;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Provider == null || Provider == null)
+            {
+                return Page();
+            }
+
+            _context.Providers.Add(Provider);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

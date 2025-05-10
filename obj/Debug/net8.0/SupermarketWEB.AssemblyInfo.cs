5dd3a970c9f4caac10f31bd68ddbfc9966f36514
EmySalazar-G1 <emelysala8@gmail.com>
2025-05-10 13:47:38 -0500

    Html Delete Providers

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254c774e971c54f247512b68df96e3307f6a4b5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21bad857e4e8bb8bcc7f02da91a4ccc21b2045a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

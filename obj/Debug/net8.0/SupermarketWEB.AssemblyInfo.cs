bbd7e31928793748f1075ef0a025c28bae99ad99
Emy Salazar-G1 <emelysala8@gmail.com>
2025-05-15 23:17:06 -0500

    Html _LoginStatusPartial.cshtml

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8bd682949d9157fc2ebcdfc32e0eb141c1712b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875e5f99a4706f0ff752d0522564f0e4b2a4616d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
